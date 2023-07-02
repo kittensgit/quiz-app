9623c89214077196fdff29b96caa8c0a7ea5fff8
Nika <nikaonime@gmail.com>
2023-07-02 13:59:00 +0300

    actions type, actions, reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,3 +1,5 @@
+import { CHANGE_AMOUNT, CHANGE_CATEGORY, CHANGE_DIFFICULTY, CHANGE_SCORE, CHANGE_TYPE } from "./actionsTypes"
+
 const initialState = {
     question_category: '',
     question_difficulty: '',
@@ -7,7 +9,30 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    return state
+    switch (action.type) {
+        case CHANGE_CATEGORY:
+            return {
+                ...state, question_category: action.payload
+            }
+        case CHANGE_DIFFICULTY:
+            return {
+                ...state, question_difficulty: action.payload
+            }
+        case CHANGE_TYPE:
+            return {
+                ...state, question_type: action.payload
+            }
+        case CHANGE_AMOUNT:
+            return {
+                ...state, amount_of_question: action.payload
+            }
+        case CHANGE_SCORE:
+            return {
+                ...state, score: action.payload
+            }
+        default:
+            return state
+    }
 }
 
 export default reducer
