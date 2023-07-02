6e0f452ef13cbb3da54e18692958dc58c91efafd
Nika <nikaonime@gmail.com>
2023-07-02 13:48:14 +0300

    createStore, reducer, useSelector

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,8 +1,17 @@
 import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
 import useAxios from '../hooks/useAxios'
+import { useSelector } from 'react-redux'
 
 const Questions = () => {
+
+    const {
+        question_category,
+        question_difficulty,
+        question_type,
+        amount_of_question,
+        score
+    } = useSelector(state => state)
 
     let apiUrl = `/api.php?amount=10`
 
