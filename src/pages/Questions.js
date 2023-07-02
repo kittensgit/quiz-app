e2511c8585174ed656546173699e213cd1087ff5
Nika <nikaonime@gmail.com>
2023-07-02 13:38:44 +0300

    Questions page

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,8 +1,28 @@
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
+import useAxios from '../hooks/useAxios'
 
 const Questions = () => {
+
+    let apiUrl = `/api.php?amount=10`
+
+    const { response, error, loading } = useAxios({ url: apiUrl })
+    console.log(response)
+
     return (
-        <div>Questions</div>
+        <Box>
+            <Typography variant='h4'>Questions 1</Typography>
+            <Typography mt={5}>This is Questions</Typography>
+            <Box mt={2}>
+                <Button variant='contained'>Answer 1</Button>
+            </Box>
+            <Box mt={2}>
+                <Button variant='contained'>Answer 2</Button>
+            </Box>
+            <Box mt={5}>
+                Score: 2/6
+            </Box>
+        </Box>
     )
 }
 
