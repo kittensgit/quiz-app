aa15792c3944f7aee079142976406523e9cd052a
Nika <nikaonime@gmail.com>
2023-07-02 12:02:48 +0300

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Settings from "./pages/Settings";
+import Questions from "./pages/Questions";
+import FinalScreen from "./pages/FinalScreen";
 
 function App() {
   return (
-    <div>
-      meow
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Settings />}/>
+        <Route path='/questions' element={<Questions />}/>
+        <Route path='/score' element={<FinalScreen />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
