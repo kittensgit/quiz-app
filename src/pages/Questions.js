80b3f0114026334c5dfee6da301fb6f6f8bb916d
Nika <nikaonime@gmail.com>
2023-07-03 12:58:29 +0300

    html-entities, decode

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,84 +1,97 @@
-import { Box, Button, CircularProgress, Typography } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import useAxios from '../hooks/useAxios'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { Button, CircularProgress, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import useAxios from "../hooks/useAxios";
+import { handleScoreChange } from "../redux/actions";
+import { useNavigate } from "react-router-dom";
+import { decode } from "html-entities";
 
 const getRandomInt = (max) => {
-    return Math.floor(Math.random() * Math.floor(max))
-}
+    return Math.floor(Math.random() * Math.floor(max));
+};
 
 const Questions = () => {
-
-    const history = useNavigate()
-
     const {
         question_category,
         question_difficulty,
         question_type,
         amount_of_question,
-        score
-    } = useSelector(state => state)
+        score,
+    } = useSelector((state) => state);
+    const history = useNavigate();
+    const dispatch = useDispatch();
 
-    let apiUrl = `/api.php?amount=${amount_of_question}`
+    let apiUrl = `/api.php?amount=${amount_of_question}`;
     if (question_category) {
-        apiUrl = apiUrl.concat(`&category=${question_category}`)
+        apiUrl = apiUrl.concat(`&category=${question_category}`);
     }
     if (question_difficulty) {
-        apiUrl = apiUrl.concat(`&difficulty=${question_difficulty}`)
+        apiUrl = apiUrl.concat(`&difficulty=${question_difficulty}`);
     }
     if (question_type) {
-        apiUrl = apiUrl.concat(`&type=${question_type}`)
+        apiUrl = apiUrl.concat(`&type=${question_type}`);
     }
 
-    const { response, error, loading } = useAxios({ url: apiUrl })
-    const [questionIndex, setQuestionIndex] = useState(0)
-    const [options, setOptions] = useState([])
-    console.log(options)
+    const { response, loading } = useAxios({ url: apiUrl });
+    const [questionIndex, setQuestionIndex] = useState(0);
+    const [options, setOptions] = useState([]);
 
     useEffect(() => {
         if (response?.results.length) {
-            const question = response.results[questionIndex]
-            let answers = [...question.incorrect_answers]
-            answers.slice(
+            const question = response.results[questionIndex];
+            let answers = [...question.incorrect_answers];
+            answers.splice(
                 getRandomInt(question.incorrect_answers.length),
                 0,
                 question.correct_answer
-            )
-            setOptions(answers)
+            );
+            setOptions(answers);
         }
-    }, [response, questionIndex])
+    }, [response, questionIndex]);
 
     if (loading) {
         return (
             <Box mt={20}>
                 <CircularProgress />
             </Box>
-        )
+        );
     }
 
-    const handleClickAnswer = () => {
-        if (questionIndex + 1 < response.results.length) {
-            setQuestionIndex(questionIndex + 1)
-        } else {
-            history('/score')
+    const handleClickAnswer = (e) => {
+        const question = response.results[questionIndex];
+        if (e.target.textContent === question.correct_answer) {
+            dispatch(handleScoreChange(score + 1));
         }
-    }
+
+        if (questionIndex + 1 < response.results.length) {
+            setQuestionIndex(questionIndex + 1);
+        } else {
+            history("/score");
+        }
+    };
+
+    console.log(response.results[questionIndex])
 
     return (
         <Box>
-            <Typography variant='h4'>Questions {questionIndex + 1}</Typography>
-            <Typography mt={5}>{response.results[questionIndex].question}</Typography>
+            <Typography variant="h4">Questions {questionIndex + 1}</Typography>
+            <Typography mt={5}>
+                {decode(response.results[questionIndex]?.question)}
+            </Typography>
             {options.map((data, id) => (
                 <Box mt={2} key={id}>
-                    <Button onClick={handleClickAnswer} variant='contained'>{data}</Button>
+                    <Button onClick={handleClickAnswer} variant="contained">
+                        {decode(data)}
+                    </Button>
                 </Box>
             ))}
             <Box mt={5}>
-                Score: {score}/{response.results.length}
+                Score: {score} / {response.results.length}
             </Box>
         </Box>
-    )
-}
+    );
+};
 
-export default Questions
+export default Questions;
